b81ce5c2a7788d6b893b1b0f4b3a36fed35bdb36
Gonzalo Maldonado <elg0nz@gmail.com>
2016-11-12 13:13:54 -0800

    Change words to swap

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,47 +1,48 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+  var child, next;
+  if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+      || node.classList.indexOf('ace_editor') > -1) {
+    return;
+  }
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  switch ( node.nodeType ) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+function handleText(textNode) {
+  var v = textNode.nodeValue;
+  var replacement = "White 💩 Supremacy";
+
+  // TODO: Refactor to make it easier to add more words and handle variations.
+  v = v.replace(/\bAlt Right\b/g, replacement);
+  v = v.replace(/\balt right\b/g, replacement);
+  v = v.replace(/\balt-right\b/g, replacement);
+  v = v.replace(/\bAlt-Right\b/g, replacement);
+  v = v.replace(/\bWhite Nationalism\b/g, replacement);
+  v = v.replace(/\bwhite nationalism\b/g, replacement);
+
+  textNode.nodeValue = v;
 }
 
 
